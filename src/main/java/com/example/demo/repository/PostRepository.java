77748814a008d57b20d3ccbe607aabd18d340385
thebestWasabi <openpw@icloud.com>
2022-12-09 16:17:02 +0300

    0.0.2

---
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Post;
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // SELECT * FROM POST as p WHERE User='user' SORT DESC
+    List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+
+    List<Post> findAllByOrderByCreatedDateDesc();
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+
+}
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -10,4 +10,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+
 }
